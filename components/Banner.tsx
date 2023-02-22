b49b82d28d8b4190dfdacf4362af2b849247c555
alberorana@gmail.com <alberorana@gmail.com>
2023-02-22 10:34:55 +0900

    apkbuild

---
+++ components/Banner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Platform } from "react-native";
+import { BannerAd, BannerAdSize, TestIds } from "react-native-google-mobile-ads";
+
+export const Banner = () => {
+
+  const productionID = Platform.select({
+    ios: "test",
+    android: "ca-app-pub-2677960012216792/2858421416"
+  })
+  
+  return (
+    <BannerAd
+      unitId={TestIds.BANNER}
+      size={BannerAdSize.BANNER}
+      requestOptions={{ requestNonPersonalizedAdsOnly: true}}
+    />
+  )
+}
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -14,6 +14,7 @@ import { createTable, select } from '../helpers/sqlite';
 
 import { Diaries } from '../components/Diaries';
 import { hourMessage } from '../helpers/fuctions';
+import { Banner } from '../components/Banner';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
   const colorScheme = useColorScheme();
@@ -101,6 +102,7 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
     >
       <ImageBackground source={backgroundSource} resizeMode="cover" style={gstyle.bgimage}>
       {/* HTMLで言うと、Sectionや/DIVタグに近い */}
+        {/* <Banner/> */}
         <View style={styles.welcome}>
           <View style={{
             flex: 1,
